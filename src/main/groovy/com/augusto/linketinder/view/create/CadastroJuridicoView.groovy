bc0f41503934a6e7f3b7927525bea88d0ff56c31
AugustoSodre <augustolima73@yahoo.com>
2025-09-07 21:52:43 -0300

    feat: Cadastro de Empresas no sistema, com verificacao usando REGEX

--- src/main/groovy/com/augusto/linketinder/view/create/CadastroJuridicoView.groovy
+++ src/main/groovy/com/augusto/linketinder/view/create/CadastroJuridicoView.groovy
@@ -1,8 +1,52 @@
 package com.augusto.linketinder.view.create
 
+import com.augusto.linketinder.control.CadastroController
+import com.augusto.linketinder.model.lista.EnumCompetencias
+import com.augusto.linketinder.model.lista.ListaJuridicaEstatica
+import com.augusto.linketinder.model.pessoa.PessoaJuridica
+
 class CadastroJuridicoView {
 
-    void show(){
-        println "Cadastro Juridico"
+    CadastroController cadastroController = new CadastroController()
+
+    void show() {
+        println "Cadastro da Pessoa Jurídica"
+        println()
+
+        PessoaJuridica pessoa = new PessoaJuridica()
+
+        print("Digite o Nome da Empresa: ")
+        pessoa.nome = cadastroController.getNomeInput()
+
+        print("Digite o Email da Empresa: ")
+        pessoa.email = cadastroController.getEmailInput()
+
+        print("Digite o Estado da Empresa: ")
+        pessoa.estado = cadastroController.getEstadoInput()
+
+        print("Digite o CEP da Empresa: ")
+        pessoa.cep = cadastroController.getCepInput()
+
+        print("Digite a Descrição da Empresa: ")
+        pessoa.descricao = cadastroController.getDescricaoInput()
+
+        print("Digite o CNPJ da Empresa: ")
+        pessoa.cnpj = cadastroController.getCnpjInput()
+
+        print("Digite o País da Empresa: ")
+        pessoa.pais = cadastroController.getPaisInput()
+
+        println()
+        println("Competências disponíveis:")
+        EnumCompetencias.values().eachWithIndex { comp, idx ->
+            println("${idx + 1} - ${comp}")
+        }
+        pessoa.competencias = cadastroController.getCompetenciasInput()
+
+        // Adiciona pessoa ao final do processo
+        ListaJuridicaEstatica.getLista().add(pessoa)
+
+        println()
+        println("Empresa adicionada com sucesso!")
     }
 }
