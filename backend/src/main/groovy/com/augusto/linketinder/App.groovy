3e12b70e2c5b8d519ef6e00d460ba129626a7124
AugustoSodre <augustolima73@yahoo.com>
2025-10-09 12:21:42 -0300

    fix: build.gradle dependencies

--- backend/src/main/groovy/com/augusto/linketinder/App.groovy
+++ backend/src/main/groovy/com/augusto/linketinder/App.groovy
@@ -5,8 +5,6 @@ import com.augusto.linketinder.view.menu.MenuMain
 
 static void main(String[] args){
 
-//    new ScriptPopularDadosInicial().start()
-
     MenuMain menuMain = new MenuMain()
 
     while (true){
